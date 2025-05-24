fbeaa793418e4f61a8e5c6ef9a24f26119db1bb3
Mark Marilag <mark.marilag27@gmail.com>
2025-05-24 13:26:33 +0800

    fix: enable security

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -9,22 +9,22 @@ const RATE_LIMIT_INTERVAL_MS = 3000
 
 export const verifyRecaptcha: MiddlewareHandler = async (c, next) => {
   const { RECAPTCHA_SECRET } = env<{ RECAPTCHA_SECRET: string }>(c)
-  // const { recaptchaToken } = await c.req.json()
+  const { recaptchaToken } = await c.req.json()
 
-  // if (!recaptchaToken) return c.json({ error: 'Missing reCAPTCHA token' }, 400)
+  if (!recaptchaToken) return c.json({ error: 'Missing reCAPTCHA token' }, 400)
 
-  // const res = await fetch('https://www.google.com/recaptcha/api/siteverify', {
-  //   method: 'POST',
-  //   body: new URLSearchParams({
-  //     secret: RECAPTCHA_SECRET,
-  //     response: recaptchaToken,
-  //   }),
-  // })
+  const res = await fetch('https://www.google.com/recaptcha/api/siteverify', {
+    method: 'POST',
+    body: new URLSearchParams({
+      secret: RECAPTCHA_SECRET,
+      response: recaptchaToken,
+    }),
+  })
 
-  // const result = await res.json() as RecaptchaSiteVerifyResponse
-  // if (!result.success || (result?.score ?? 0) < 0.5) {
-  //   return c.json({ error: 'Failed reCAPTCHA check' }, 403)
-  // }
+  const result = await res.json() as RecaptchaSiteVerifyResponse
+  if (!result.success || (result?.score ?? 0) < 0.5) {
+    return c.json({ error: 'Failed reCAPTCHA check' }, 403)
+  }
 
   await next()
 }
@@ -47,16 +47,16 @@ export const protectAndLimit: MiddlewareHandler = async (c, next) => {
     return c.json({ error: 'Too many requests – slow down' }, 429)
   }
 
-  // const date = new Date().toISOString().slice(0, 10)
-  // const dailyKey = `quota:${email}:${date}`
-  // const count = parseInt((await kv.get(dailyKey)) || '0')
+  const date = new Date().toISOString().slice(0, 10)
+  const dailyKey = `quota:${email}:${date}`
+  const count = parseInt((await kv.get(dailyKey)) || '0')
 
-  // if (count >= LIMIT_PER_DAY) {
-  //   return c.json({ error: 'Daily quota reached. Try again tomorrow.' }, 403)
-  // }
+  if (count >= LIMIT_PER_DAY) {
+    return c.json({ error: 'Daily quota reached. Try again tomorrow.' }, 403)
+  }
 
-  // await kv.put(lastKey, now.toString(), { expirationTtl: 60 }) // Expires in 60s
-  // await kv.put(dailyKey, (count + 1).toString(), { expirationTtl: 86400 }) // Expires in 24h
+  await kv.put(lastKey, now.toString(), { expirationTtl: 60 }) // Expires in 60s
+  await kv.put(dailyKey, (count + 1).toString(), { expirationTtl: 86400 }) // Expires in 24h
 
   await next()
 }
